e53512317d3d8f93d8a9302b8ed994bd0855077e
Elizabeth Langenstein <elizabeth.langenstein@kellogg.northwestern.edu>
2024-04-19 14:37:22 +0000

    class part 1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,50 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+# puts 5
+# puts 2
+# puts 2+5
+# puts 5*2 
+# puts 5-2
+# puts 5/2.0
+# puts (5-2)*3
 
-# Non-numbers
+# # Non-numbers
+# puts "Hello, world!"
+# puts "Tacos are " + "delish"
+# puts "tacos" * 3
+
+# puts 3*"tacos" - have to do the *3 after text
+
+
+# # Variables
+# x=5
+# y=2
+
+# zebra = 8
+
+# puts zebra*x
+
+# food = "tacos"
+# quantity = 3 
+# puts food*quantity
+
+# Combine strings and variables
+# puts "i want 3 tacos"
+# puts "i want #{quantity} tacos"
+
+# puts "I want #{quantity} #{food}"
+
+# name = "Ben"
+# puts "hello, #{name}!"
+# String manipulation
+
 
 # True, False
 
+# puts true 
+# puts false 
+
+puts 3 == 2 
+
 # Nothing
-
-# Variables
-
-# Combine strings and variables
-
-# String manipulation
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,12 +4,60 @@
 
 # Booleans
 
+
 # Boolean Expressions
+value1 = 3
+value2 = 2 
+
+# puts value1 == value2
+# puts value1 != value2
+# puts value1 > value2
+# puts value1 <= value2
+# puts value1 >= value2
+
 
 # If Conditional Logic
+# if value1 == value2
+#     puts 'math is broken!'
+# end 
+# if value1 != value2
+#     puts 'phew'
+# end 
 
 # If/Else Conditional Logic
+# if value1 == value2
+#     puts "math is broken!"
+# else
+#     puts "math works"
+# end
 
+# user_entered_password = "tacos"
+# real_password = "secret"
+
+# if user_entered_password == real_password
+#     puts "great youre logged in"
+# else
+#     puts "wrong password"
+# end 
 # Elsif Conditional Logic
+home_team = 2 
+away_team = 2
+
+if home_team > away_team
+    puts "we won!"
+elsif home_team == away_team
+    puts "we tied"
+else
+    puts "we lost"
+end
 
 # Combining Expressions
+temp = 60 
+if temp >= 65 && temp <= 80
+    puts "its nice"
+else
+    puts "ughhh"
+end 
+
+# || --> OR
+#  && --> and 
